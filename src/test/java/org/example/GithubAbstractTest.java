9cb991c1ef35f938e1bff7875024804c6d89b596
Your Name <you@example.com>
2025-02-01 16:13:12 +0400

    Обновление кода

--- src/test/java/org/example/GithubAbstractTest.java
+++ src/test/java/org/example/GithubAbstractTest.java
@@ -14,20 +14,17 @@ public class GithubAbstractTest {
     static void initTest() {
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
 
-        // Загружаем переменные окружения из .env
-        Dotenv dotenv = Dotenv.load();
-
-        // Получаем значения из .env
-        apiKey = dotenv.get("GITHUB_API_KEY");
-        baseUrl = dotenv.get("GITHUB_BASE_URL");
-    }
-
-    public static String getApiKey() {
-        return apiKey;
-    }
-
-    public static String getBaseUrl() {
-        return baseUrl;
+        // Загружаем .env, если он есть
+        Dotenv dotenv;
+        try {
+            dotenv = Dotenv.load();
+            apiKey = dotenv.get("GITHUB_API_KEY");
+            baseUrl = dotenv.get("GITHUB_BASE_URL");
+        } catch (Exception e) {
+            System.out.println("Файл .env не найден, используем переменные окружения");
+            apiKey = System.getenv("GITHUB_API_KEY");
+            baseUrl = System.getenv("GITHUB_BASE_URL");
+        }
     }
 
 }
